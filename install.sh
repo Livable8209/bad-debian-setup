90567e6bda39da6ed8ae268ea5d547350b442e3f
Cluster <136758278+Livable8209@users.noreply.github.com>
2024-02-03 15:42:05 -0500

    add source print

--- install.sh
+++ install.sh
@@ -47,3 +47,5 @@ curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
 # shellcheck disable=SC1091
 source "$HOME/.cargo/env"
 cargo install bore-cli
+
+echo "restart your shell or run 'source .bashrc'" 
